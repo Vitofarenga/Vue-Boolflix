a118cf184555e6f6ee759b793c1933dfdcf40f3f
Vito Farenga <vitofarenga@libero.it>
2022-07-21 16:02:01 +0200

    preparazione

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+   <HeaderCli />
+   <MainCli />
+   <FooterCli />
+  </div>
+</template>
+
+<script>
+
+import HeaderCli from './components/HeaderCli.vue'
+import MainCli from './components/MainCli.vue'
+import FooterCli from './components/FooterCli.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderCli,
+    MainCli,
+    FooterCli,
+  }
+}
+</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
